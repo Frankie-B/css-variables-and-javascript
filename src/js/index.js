e40942021bf81fb4ec2698c730b8ae775aa77370
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-22 22:31:13 +0300

    Add finished code

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,6 @@ const inputs = document.querySelectorAll('.controls input');
 // Function for updating css variables
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
-  console.log(suffix);
 
   //   changing our css variables
   document.documentElement.style.setProperty(
