8ae2f66bc80bed0e01ca8cbbc7991f8c78c3ccac
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-22 22:08:29 +0300

    Add comments to updateHnadle

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@
 
 const inputs = document.querySelectorAll('.controls input');
 
+// Function for updating css variables
 function handleUpdate() {
   console.log(this.value);
 }
 
+// Event listeners for changes and mouse moves
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
