bd96c62b6be13238147a2848731952135daaca67
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-22 22:04:01 +0300

    Add Event Listener for change event

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 // jshint esversion: 6
 
 const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
